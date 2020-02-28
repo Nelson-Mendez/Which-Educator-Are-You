edc95031b26f2e3caffb82d97535a2d6dab56a0f
Nelson Mendez <nelmen2509@gmail.com>
2020-02-28 14:53:49 -0500

    Updated the question bank

--- which-ta-are-you/client/src/data/questionBank.js
+++ which-ta-are-you/client/src/data/questionBank.js
@@ -13,23 +13,35 @@ var quizQuestions = [
             {
                 type: "Rakesh",
                 content: "Roy Lee Jr"
+            },
+            {
+                type: "Amy",
+                content: "Dog"
+            },
+            {
+                type: "Roy Lee Jr",
+                content: "Panda"
             }
         ]
     },
     {
-        question: "What is your favourite word in this list?",
+        question: "What is your favourite phrase in this list?",
         answers: [
             {
                 type: "Josh",
-                content: "Trash"
+                content: "This is trash, but..."
             },
             {
                 type: "Daniil",
-                content: "Contrived"
+                content: "This is a contrived example"
             },
             {
                 type: "Rakesh",
-                content: "Roy Lee Jr"
+                content: "We're gonna name this Roy Lee Jr"
+            },
+            {
+                type: "Roy Lee Jr",
+                content: "Does this make sense to everyone?"
             }
         ]
     },
@@ -37,7 +49,7 @@ var quizQuestions = [
         question: "What do you like to eat?",
         answers: [
             {
-                type: "Josh",
+                type: "Roy Lee Jr",
                 content: "Yuki Sushi"
             },
             {
@@ -47,6 +59,14 @@ var quizQuestions = [
             {
                 type: "Rakesh",
                 content: "Whatever Roy Lee Jr is eating"
+            },
+            {
+                type: "Amy",
+                content: "Burritos"
+            },
+            {
+                type: "Josh",
+                content: "McDonalds"
             }
         ]
     },
@@ -54,7 +74,7 @@ var quizQuestions = [
         question: "Favourite smell?",
         answers: [
             {
-                type: "Josh",
+                type: "Roy Lee Jr",
                 content: "A hot dawg"
             },
             {
@@ -64,6 +84,10 @@ var quizQuestions = [
             {
                 type: "Rakesh",
                 content: "Roy Lee Jr's sweater"
+            },
+            {
+                type: "Amy",
+                content: "White Board Markers"
             }
         ]
     },
@@ -76,7 +100,7 @@ var quizQuestions = [
             },
             {
                 type: "Daniil",
-                content: "Refactor the crap out of it"
+                content: "Refactor it"
             },
             {
                 type: "Rakesh",
@@ -85,6 +109,160 @@ var quizQuestions = [
             {
                 type: "Amy",
                 content: "Talk about how to solve it out loud"
+            },
+            {
+                type: "Roy Lee Jr",
+                content: "Uhhhhh I see I see...."
+            }
+        ]
+    },
+    {
+        question: "Favorite TV Show?",
+        answers: [
+            {
+                type: "Josh",
+                content: "Jojo's Bizarre Adventure"
+            },
+            {
+                type: "Daniil",
+                content: "Chernobyl"
+            },
+            {
+                type: "Rakesh",
+                content: "Simpsons"
+            },
+            {
+                type: "Amy",
+                content: "Netflix"
+            },
+            {
+                type: "Roy Lee Jr",
+                content: "Yu.....Tube"
+            }
+        ]
+    },
+    {
+        question: "What time did you go to sleep last night?",
+        answers: [
+            {
+                type: "Josh",
+                content: "2 am I think?"
+            },
+            {
+                type: "Daniil",
+                content: "Sleep is for the weak"
+            },
+            {
+                type: "Rakesh",
+                content: "I haven't slept in days"
+            },
+            {
+                type: "Amy",
+                content: "10pm sharp"
+            },
+            {
+                type: "Roy Lee Jr",
+                content: "Midnight"
+            }
+        ]
+    },
+    {
+        question: "Which House are you?",
+        answers: [
+            {
+                type: "Josh",
+                content: "Gryffindor"
+            },
+            {
+                type: "Daniil",
+                content: "Slytherin"
+            },
+            {
+                type: "Rakesh",
+                content: "Ravenclaw"
+            },
+            {
+                type: "Amy",
+                content: "Hufflepuff"
+            },
+            {
+                type: "Roy Lee Jr",
+                content: "House Elf"
+            }
+        ]
+    },
+    {
+        question: "Favorite Season?",
+        answers: [
+            {
+                type: "Josh",
+                content: "Winter"
+            },
+            {
+                type: "Daniil",
+                content: "Coding Season"
+            },
+            {
+                type: "Rakesh",
+                content: "Autumn"
+            },
+            {
+                type: "Amy",
+                content: "Spring"
+            },
+            {
+                type: "Roy Lee Jr",
+                content: "Summer"
+            }
+        ]
+    },
+    {
+        question: "Pick an emoji!",
+        answers: [
+            {
+                type: "Josh",
+                content: "🦎"
+            },
+            {
+                type: "Daniil",
+                content: "🍆"
+            },
+            {
+                type: "Rakesh",
+                content: "💩"
+            },
+            {
+                type: "Amy",
+                content: "😇"
+            },
+            {
+                type: "Roy Lee Jr",
+                content: "🗡"
+            }
+        ]
+    },
+    {
+        question: "Finally, which is your spirit animal?",
+        answers: [
+            {
+                type: "Josh",
+                content: "Sloth"
+            },
+            {
+                type: "Daniil",
+                content: "Apparently Kid"
+            },
+            {
+                type: "Rakesh",
+                content: "Roy Lee Jr"
+            },
+            {
+                type: "Amy",
+                content: "Quokka"
+            },
+            {
+                type: "Roy Lee Jr",
+                content: "Yash"
             }
         ]
     }
